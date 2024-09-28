3e1193dad407210aa69f097ec344170b46e653b4
Joao P Dubas <joao.dubas@gmail.com>
2024-09-28 10:43:48 +0000

    chore: define `spec` before `doc`

--- lib/container/protocols/container_builder.ex
+++ lib/container/protocols/container_builder.ex
@@ -2,12 +2,16 @@ defprotocol Testcontainers.ContainerBuilder do
   @moduledoc """
   All types of predefined containers must implement this protocol.
   """
-  @spec build(t()) :: %Testcontainers.Container{}
+
+  @doc """
+  Build a container configuration based on a concrete strategy.
+  """
+  @spec build(t()) :: Testcontainers.Container.t()
   def build(builder)
 
   @doc """
   Do stuff after container has started.
   """
-  @spec after_start(t(), %Testcontainers.Container{}, %Tesla.Env{}) :: :ok
+  @spec after_start(t(), Testcontainers.Container.t(), Tesla.Env.t()) :: :ok
   def after_start(builder, container, connection)
 end
